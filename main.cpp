a43455645cb11a32ba9725182d9afc8712451d12
Andersson1901 <anderssonalvarado4@gmail.com>
2024-08-29 22:50:36 -0500

    proyecto 2

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Incluye la biblioteca estándar de entrada y salida
+
+int main() {
+    std::cout << "Hola Mundo" << std::endl; // Imprime "Hola Mundo" en la consola
+    return 0; // Indica que el programa terminó correctamente
+}
